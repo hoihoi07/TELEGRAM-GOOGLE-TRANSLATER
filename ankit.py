8f7ccb4be5138b8a714ccfc2182cc472bc4829be
⚝ANKIT KUMAR⚝ <97168788+LEGEND-ANKIT@users.noreply.github.com>
2022-01-08 11:16:10 +0530

    Update ankit.py

--- ankit.py
+++ ankit.py
@@ -20,7 +20,7 @@ app = Client(
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
-	await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("🔰DEVELOPER🔰" ,url="https://github.com/LEGEND-ANKIT") ],               [InlineKeyboardButton("🔰REPO🔰", url="https://github.com/LEGEND-ANKIT/TELEGRAM-GOOGLE-TRANSLATER") ]   ]  ) )
+	await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("🔰DEVELOPER🔰" ,url="https://github.com/LEGEND-ANKIT") ],               [InlineKeyboardButton("🔰©LEGEND-ANKIT™🔰", url="https://github.com/LEGEND-ANKIT") ]   ]  ) )
                   
 
 
