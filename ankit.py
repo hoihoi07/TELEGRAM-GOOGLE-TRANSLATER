865288689e3fa1c29470bf763ff299635b20603f
hoihoi07 <97010331+hoihoi07@users.noreply.github.com>
2022-01-25 17:38:49 +0530

    Update ankit.py

--- ankit.py
+++ ankit.py
@@ -20,7 +20,7 @@ app = Client(
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
-	await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("🔰DEVELOPER🔰" ,url="https://t.me/jettastic") ],               [InlineKeyboardButton("🔰Channel™🔰", url="https://t.me/jettastic") ]   ]  ) )
+	await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("🔰DEVELOPER🔰" ,url="https://t.me/jettastic") ],               [InlineKeyboardButton("🔰Channel™🔰", url="https://t.me/jetbots") ]   ]  ) )
                   
 
 
